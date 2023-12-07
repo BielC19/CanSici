f6e0d9f0656b62197c59f13872d20d4bf2ccc1ef
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-12-07 01:30:38 +0100

    deleted:    jsss.js 	modified:   nav_bar.html

--- jsss.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burger = document.querySelector('.burger');
-    const navbarMenu = document.querySelector('.navbar-menu');
-
-    burger.addEventListener('click', function () {
-        navbarMenu.classList.toggle('is-active');
-        console.log('Elemento burger clickeado.'); // Agregado
-    });
-});
