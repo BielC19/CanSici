548acc32b2b2d3fb83edf64956b862b17c25d1f9
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-12-08 01:00:22 +0100

    new file:   btns.js 	modified:   cardFliper.css 	modified:   index.html 	modified:   nav_bar.html 	modified:   nav_bar2.css

---
+++ btns.js
@@ -0,0 +1,27 @@
+function HFotos() {
+    window.open('./HFotos', '_top');
+};
+function HCasa() {
+    window.open('./HCasa', '_top');
+}
+function HEntron() {
+    window.open('./HEntron', '_top');
+}
+function HArribar() {
+    window.open('./HArribar', '_top');
+}
+function HCalendari() {
+    window.open('./HCalendari', '_top');
+}
+function HPreus() {
+    window.open('./HPreus', '_top');
+}
+function HReserves() {
+    window.open('./HReserves', '_top');
+}
+function HNormes() {
+    window.open('./HNormes', '_top');
+}
+function HContactens() {
+    window.open('./HContactens', '_top');
+}
